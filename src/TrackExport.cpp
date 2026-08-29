b9cd2eef54ab5c4fc422ff79407a3604097f3d3d
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-08-29 23:02:44 +0530

    Implement GPX and KML track export

---
+++ src/TrackExport.cpp
@@ -0,0 +1,23 @@
+#include "TrackExport.h"
+
+String trackToGpx(const GnssLogger &logger, const String &name) {
+  String out = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+  out += "<gpx version=\"1.1\" creator=\"NavIC-GPS-Bridge\" xmlns=\"http://www.topografix.com/GPX/1/1\">\n";
+  out += "<trk><name>" + name + "</name><trkseg>\n";
+  for (size_t i = 0; i < logger.size(); ++i) {
+    const auto &p = logger.at(i);
+    out += "<trkpt lat=\"" + String(p.lat, 7) + "\" lon=\"" + String(p.lon, 7) + "\"><ele>" + String(p.altitude, 2) + "</ele></trkpt>\n";
+  }
+  out += "</trkseg></trk></gpx>\n";
+  return out;
+}
+
+String trackToKml(const GnssLogger &logger, const String &name) {
+  String out = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><name>" + name + "</name><Placemark><LineString><tessellate>1</tessellate><coordinates>\n";
+  for (size_t i = 0; i < logger.size(); ++i) {
+    const auto &p = logger.at(i);
+    out += String(p.lon, 7) + "," + String(p.lat, 7) + "," + String(p.altitude, 2) + " ";
+  }
+  out += "\n</coordinates></LineString></Placemark></Document></kml>\n";
+  return out;
+}
