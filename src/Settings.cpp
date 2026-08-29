e971ed295f59bb7122486936d85bdd61ae6e784f
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-08-29 20:58:27 +0530

    Implement persistent configuration storage

---
+++ src/Settings.cpp
@@ -0,0 +1,32 @@
+#include "Settings.h"
+
+bool SettingsManager::begin() {
+  if (!prefs.begin("navicbridge", false)) return false;
+  config.gnssBaud = prefs.getULong("gnssBaud", 9600);
+  config.gpsOutBaud = prefs.getULong("outBaud", 9600);
+  config.tcpPort = prefs.getUShort("tcpPort", 10110);
+  config.gpsCompatibility = prefs.getBool("gpsCompat", true);
+  config.apEnabled = prefs.getBool("apEnabled", true);
+  config.staEnabled = prefs.getBool("staEnabled", false);
+  config.staSsid = prefs.getString("staSsid", "");
+  config.staPassword = prefs.getString("staPass", "");
+  return true;
+}
+
+bool SettingsManager::save(const BridgeConfig &next) {
+  config = next;
+  prefs.putULong("gnssBaud", config.gnssBaud);
+  prefs.putULong("outBaud", config.gpsOutBaud);
+  prefs.putUShort("tcpPort", config.tcpPort);
+  prefs.putBool("gpsCompat", config.gpsCompatibility);
+  prefs.putBool("apEnabled", config.apEnabled);
+  prefs.putBool("staEnabled", config.staEnabled);
+  prefs.putString("staSsid", config.staSsid);
+  prefs.putString("staPass", config.staPassword);
+  return true;
+}
+
+void SettingsManager::reset() {
+  prefs.clear();
+  config = BridgeConfig();
+}
