e6f498adfeb9806b1b609a77eddfcb2a95c6f0b9
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-08-29 20:58:20 +0530

    Add persistent configuration manager

---
+++ src/Settings.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <Arduino.h>
+#include <Preferences.h>
+
+struct BridgeConfig {
+  uint32_t gnssBaud = 9600;
+  uint32_t gpsOutBaud = 9600;
+  uint16_t tcpPort = 10110;
+  bool gpsCompatibility = true;
+  bool apEnabled = true;
+  bool staEnabled = false;
+  String staSsid;
+  String staPassword;
+};
+
+class SettingsManager {
+ public:
+  bool begin();
+  const BridgeConfig &get() const { return config; }
+  bool save(const BridgeConfig &next);
+  void reset();
+ private:
+  Preferences prefs;
+  BridgeConfig config;
+};
