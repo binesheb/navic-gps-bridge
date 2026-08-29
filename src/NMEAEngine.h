1239b8039ba052cf27cebbc2160efb07bbd89da3
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-08-29 20:20:26 +0530

    Add satellite data model and GSV support

--- src/NMEAEngine.h
+++ src/NMEAEngine.h
@@ -1,29 +1,5 @@
 #pragma once
 #include <Arduino.h>
-
-struct GnssData {
-  bool valid = false;
-  bool fix = false;
-  int fixQuality = 0;
-  double latitude = 0;
-  double longitude = 0;
-  double altitude = 0;
-  double speedKmh = 0;
-  double course = 0;
-  int satellites = 0;
-  double hdop = 0;
-  String utcTime;
-  String utcDate;
-  String lastSentence;
-};
-
-class NMEAEngine {
- public:
-  bool process(const String &line);
-  const GnssData &data() const { return state; }
-  bool checksumValid(const String &line) const;
-  String gpsCompatible(const String &line) const;
- private:
-  GnssData state;
-  double parseCoordinate(const String &value, const String &hemisphere) const;
-};
+struct GnssData{bool valid=false,fix=false;int fixQuality=0,satellites=0;double latitude=0,longitude=0,altitude=0,speedKmh=0,course=0,hdop=0;String utcTime,utcDate,lastSentence;};
+struct SatelliteData{String constellation;int prn=0,snr=0,elevation=0,azimuth=0;};
+class NMEAEngine{public:bool process(const String&);const GnssData&data()const{return state;}const SatelliteData*satellites()const{return sats;}int satelliteCount()const{return satCount;}bool checksumValid(const String&)const;String gpsCompatible(const String&)const;private:GnssData state;SatelliteData sats[32];int satCount=0;double parseCoordinate(const String&,const String&)const;void parseGsv(const String&,const String&);};
