66cd8cec0bebf0a1bf4a5439160df03df2d4a11b
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-08-30 10:58:30 +0530

    Add CSV track export support

--- src/TrackExport.h
+++ src/TrackExport.h
@@ -2,5 +2,6 @@
 #include <Arduino.h>
 #include "GnssLogger.h"
 
+String trackToCsv(const GnssLogger &logger);
 String trackToGpx(const GnssLogger &logger, const String &name = "NavIC GPS Bridge Track");
 String trackToKml(const GnssLogger &logger, const String &name = "NavIC GPS Bridge Track");
