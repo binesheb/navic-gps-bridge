ad6f5ea5c805c91c285525ea2d85df5fee46bcfd
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-08-30 12:58:13 +0530

    Wire EventEngine to bounded event history

--- src/EventEngine.h
+++ src/EventEngine.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Arduino.h>
 #include "NMEAEngine.h"
+#include "EventLog.h"
 
 enum class AlertType { FixLost, FixAcquired, HighHdop, LowSatellites };
 
@@ -15,6 +16,9 @@ class EventEngine {
   bool update(const GnssData &data, bool fresh, unsigned long now);
   const AlertEvent &last() const { return event; }
   unsigned long count() const { return eventCount; }
+  const EventLog &history() const { return log; }
+  EventLog &history() { return log; }
+  void clearHistory() { log.clear(); }
  private:
   bool initialized = false;
   bool previousFix = false;
@@ -22,5 +26,6 @@ class EventEngine {
   bool satelliteAlert = false;
   AlertEvent event{AlertType::FixLost,0,""};
   unsigned long eventCount = 0;
+  EventLog log;
   void emit(AlertType type, unsigned long now, const String &message);
 };
