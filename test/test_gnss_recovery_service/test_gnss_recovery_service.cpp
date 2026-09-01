121c44a8bbb2c90f0312d96dcee140b537e8c2af
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-09-02 02:59:06 +0530

    test(gnss): cover live recovery policy reconfiguration

--- test/test_gnss_recovery_service/test_gnss_recovery_service.cpp
+++ test/test_gnss_recovery_service/test_gnss_recovery_service.cpp
@@ -52,11 +52,38 @@ void test_service_attaches_live_recovery_status() {
   TEST_ASSERT_EQUAL_UINT32(100, counters.gnssRecovery->lastRecoveryMs);
 }
 
+void test_service_reconfigures_policy_without_resetting_history() {
+  BridgeConfig config;
+  config.gnssRecoverySilenceMs = 100;
+  config.gnssRecoveryCooldownMs = 1000;
+
+  GnssRecoveryService service(config);
+  service.begin(0);
+
+  GnssRecoveryAction action;
+  TEST_ASSERT_TRUE(service.controller().poll(100, action));
+  TEST_ASSERT_EQUAL_UINT32(1, service.controller().status().recoveryCount);
+
+  config.gnssRecoverySilenceMs = 500;
+  config.gnssRecoveryCooldownMs = 200;
+  service.reconfigure(config);
+
+  TEST_ASSERT_EQUAL_UINT32(500, service.controller().status().silenceMs);
+  TEST_ASSERT_EQUAL_UINT32(200, service.controller().status().cooldownMs);
+  TEST_ASSERT_EQUAL_UINT32(1, service.controller().status().recoveryCount);
+  TEST_ASSERT_EQUAL_UINT32(100, service.controller().status().lastRecoveryMs);
+
+  TEST_ASSERT_FALSE(service.controller().poll(299, action));
+  TEST_ASSERT_TRUE(service.controller().poll(301, action));
+  TEST_ASSERT_EQUAL_UINT32(2, service.controller().status().recoveryCount);
+}
+
 void setup() {
   UNITY_BEGIN();
   RUN_TEST(test_service_uses_persisted_recovery_policy);
   RUN_TEST(test_service_marks_fresh_data);
   RUN_TEST(test_service_attaches_live_recovery_status);
+  RUN_TEST(test_service_reconfigures_policy_without_resetting_history);
   UNITY_END();
 }
 
