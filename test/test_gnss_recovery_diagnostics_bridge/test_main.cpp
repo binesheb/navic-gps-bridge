759191f3b7a5eb8a236afaf2750cf4354e1321d4
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-09-01 12:00:29 +0530

    test(gnss): cover recovery diagnostics bridge

---
+++ test/test_gnss_recovery_diagnostics_bridge/test_main.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <unity.h>
+#include "GnssRuntimeDiagnostics.h"
+
+void test_recovery_diagnostics_attaches_live_controller_status() {
+  GnssRecoveryController controller(1000, 5000);
+  GnssRecoveryAction action;
+  TEST_ASSERT_TRUE(controller.poll(1101, 100, action));
+
+  LiveDiagnosticsCounters counters;
+  attachGnssRecoveryDiagnostics(counters, controller);
+
+  TEST_ASSERT_NOT_NULL(counters.gnssRecovery);
+  TEST_ASSERT_TRUE(counters.gnssRecovery->recovering);
+  TEST_ASSERT_EQUAL_UINT32(1, counters.gnssRecovery->recoveryCount);
+  TEST_ASSERT_EQUAL_UINT32(1101, counters.gnssRecovery->lastRecoveryMs);
+  TEST_ASSERT_EQUAL_UINT32(100, counters.gnssRecovery->lastDataMs);
+  TEST_ASSERT_EQUAL_UINT32(1000, counters.gnssRecovery->silenceMs);
+}
+
+void test_recovery_diagnostics_clears_when_data_returns() {
+  GnssRecoveryController controller(1000, 5000);
+  GnssRecoveryAction action;
+  TEST_ASSERT_TRUE(controller.poll(1101, 100, action));
+  controller.markData(1200);
+
+  LiveDiagnosticsCounters counters;
+  attachGnssRecoveryDiagnostics(counters, controller);
+
+  TEST_ASSERT_NOT_NULL(counters.gnssRecovery);
+  TEST_ASSERT_FALSE(counters.gnssRecovery->recovering);
+  TEST_ASSERT_EQUAL_UINT32(1200, counters.gnssRecovery->lastDataMs);
+}
+
+void setup() {
+  UNITY_BEGIN();
+  RUN_TEST(test_recovery_diagnostics_attaches_live_controller_status);
+  RUN_TEST(test_recovery_diagnostics_clears_when_data_returns);
+  UNITY_END();
+}
+
+void loop() {}
