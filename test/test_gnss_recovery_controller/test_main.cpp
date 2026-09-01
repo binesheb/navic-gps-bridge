56e26edcaa37df2e87cd6ce0664cead1f5a75f32
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-09-01 13:03:28 +0530

    test(gnss): cover self-tracked recovery polling

--- test/test_gnss_recovery_controller/test_main.cpp
+++ test/test_gnss_recovery_controller/test_main.cpp
@@ -30,11 +30,23 @@ void test_fresh_data_clears_recovery_state() {
   TEST_ASSERT_EQUAL_UINT32(1200, controller.status().lastDataMs);
 }
 
+void test_controller_can_poll_using_its_own_data_timestamp() {
+  GnssRecoveryController controller(1000, 5000);
+  GnssRecoveryAction action;
+  controller.markData(100);
+  TEST_ASSERT_FALSE(controller.poll(1000, action));
+  TEST_ASSERT_TRUE(controller.poll(1101, action));
+  TEST_ASSERT_TRUE(action.restartUart);
+  TEST_ASSERT_EQUAL_UINT32(1, action.attempt);
+  TEST_ASSERT_EQUAL_UINT32(100, controller.status().lastDataMs);
+}
+
 void setup() {
   UNITY_BEGIN();
   RUN_TEST(test_controller_requests_restart_after_silence);
   RUN_TEST(test_controller_cooldown_blocks_repeat_restart);
   RUN_TEST(test_fresh_data_clears_recovery_state);
+  RUN_TEST(test_controller_can_poll_using_its_own_data_timestamp);
   UNITY_END();
 }
 
