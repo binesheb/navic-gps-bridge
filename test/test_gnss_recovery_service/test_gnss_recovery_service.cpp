8084f7bab05cc261996d6d45cf856c2c6b363227
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-09-01 21:01:21 +0530

    test(gnss): cover recovery service diagnostics attachment

--- test/test_gnss_recovery_service/test_gnss_recovery_service.cpp
+++ test/test_gnss_recovery_service/test_gnss_recovery_service.cpp
@@ -31,10 +31,32 @@ void test_service_marks_fresh_data() {
   TEST_ASSERT_TRUE(service.controller().poll(150, action));
 }
 
+void test_service_attaches_live_recovery_status() {
+  BridgeConfig config;
+  config.gnssRecoverySilenceMs = 100;
+  config.gnssRecoveryCooldownMs = 1000;
+
+  GnssRecoveryService service(config);
+  service.begin(0);
+
+  LiveDiagnosticsCounters counters;
+  service.attachDiagnostics(counters);
+
+  TEST_ASSERT_NOT_NULL(counters.gnssRecovery);
+  TEST_ASSERT_EQUAL_UINT32(0, counters.gnssRecovery->recoveryCount);
+
+  GnssRecoveryAction action;
+  TEST_ASSERT_TRUE(service.controller().poll(100, action));
+  TEST_ASSERT_TRUE(action.restartUart);
+  TEST_ASSERT_EQUAL_UINT32(1, counters.gnssRecovery->recoveryCount);
+  TEST_ASSERT_EQUAL_UINT32(100, counters.gnssRecovery->lastRecoveryMs);
+}
+
 void setup() {
   UNITY_BEGIN();
   RUN_TEST(test_service_uses_persisted_recovery_policy);
   RUN_TEST(test_service_marks_fresh_data);
+  RUN_TEST(test_service_attaches_live_recovery_status);
   UNITY_END();
 }
 
