f983a970578e91ab6f24831a22a8025c1800781a
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-09-15 00:04:23 +0530

    test: cover GNSS fix freshness behavior

--- test/test_nmea_engine/test_main.cpp
+++ test/test_nmea_engine/test_main.cpp
@@ -29,6 +29,24 @@ void test_bad_checksum_is_rejected() {
   TEST_ASSERT_FALSE(engine.process("$GNRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*7D"));
 }
 
+void test_fix_is_fresh_immediately_after_valid_sentence() {
+  NMEAEngine engine;
+  TEST_ASSERT_TRUE(engine.process(VALID_RMC));
+  TEST_ASSERT_TRUE(engine.fixFresh(millis(), 3000));
+}
+
+void test_fix_is_not_fresh_before_first_valid_fix() {
+  NMEAEngine engine;
+  TEST_ASSERT_FALSE(engine.fixFresh(millis(), 3000));
+}
+
+void test_fix_freshness_expires_by_supplied_age() {
+  NMEAEngine engine;
+  TEST_ASSERT_TRUE(engine.process(VALID_RMC));
+  uint32_t now = millis();
+  TEST_ASSERT_FALSE(engine.fixFresh(now + 3001, 3000));
+}
+
 void setup() {
   UNITY_BEGIN();
   RUN_TEST(test_valid_checksum_is_accepted);
@@ -36,6 +54,9 @@ void setup() {
   RUN_TEST(test_short_checksum_is_rejected);
   RUN_TEST(test_non_hex_checksum_is_rejected);
   RUN_TEST(test_bad_checksum_is_rejected);
+  RUN_TEST(test_fix_is_fresh_immediately_after_valid_sentence);
+  RUN_TEST(test_fix_is_not_fresh_before_first_valid_fix);
+  RUN_TEST(test_fix_freshness_expires_by_supplied_age);
   UNITY_END();
 }
 
