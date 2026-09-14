1e45590a41a41de55840119647fb8aa7da6e9bf7
Binesh Ellupurayil Balachandran <31845423+binesheb@users.noreply.github.com>
2026-09-15 01:04:28 +0530

    test: cover GNSS source identity

--- test/test_nmea_engine/test_main.cpp
+++ test/test_nmea_engine/test_main.cpp
@@ -2,6 +2,7 @@
 #include "NMEAEngine.h"
 
 static const String VALID_RMC = "$GNRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*7C";
+static const String VALID_NAVIC_RMC = "$GIRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*73";
 
 void test_valid_checksum_is_accepted() {
   NMEAEngine engine;
@@ -29,6 +30,25 @@ void test_bad_checksum_is_rejected() {
   TEST_ASSERT_FALSE(engine.process("$GNRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*7D"));
 }
 
+void test_gnss_source_is_exposed_for_mixed_talker() {
+  NMEAEngine engine;
+  TEST_ASSERT_TRUE(engine.process(VALID_RMC));
+  TEST_ASSERT_EQUAL_STRING("GNSS", engine.data().source.c_str());
+}
+
+void test_navic_source_is_exposed_for_gi_talker() {
+  NMEAEngine engine;
+  TEST_ASSERT_TRUE(engine.process(VALID_NAVIC_RMC));
+  TEST_ASSERT_EQUAL_STRING("NavIC", engine.data().source.c_str());
+}
+
+void test_invalid_sentence_does_not_replace_source() {
+  NMEAEngine engine;
+  TEST_ASSERT_TRUE(engine.process(VALID_NAVIC_RMC));
+  TEST_ASSERT_FALSE(engine.process("$GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*00"));
+  TEST_ASSERT_EQUAL_STRING("NavIC", engine.data().source.c_str());
+}
+
 void test_fix_is_fresh_immediately_after_valid_sentence() {
   NMEAEngine engine;
   TEST_ASSERT_TRUE(engine.process(VALID_RMC));
@@ -54,6 +74,9 @@ void setup() {
   RUN_TEST(test_short_checksum_is_rejected);
   RUN_TEST(test_non_hex_checksum_is_rejected);
   RUN_TEST(test_bad_checksum_is_rejected);
+  RUN_TEST(test_gnss_source_is_exposed_for_mixed_talker);
+  RUN_TEST(test_navic_source_is_exposed_for_gi_talker);
+  RUN_TEST(test_invalid_sentence_does_not_replace_source);
   RUN_TEST(test_fix_is_fresh_immediately_after_valid_sentence);
   RUN_TEST(test_fix_is_not_fresh_before_first_valid_fix);
   RUN_TEST(test_fix_freshness_expires_by_supplied_age);
